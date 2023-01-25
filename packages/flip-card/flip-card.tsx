bcb58bae8e589ee53c71a1125ae78c6ba3ba8188
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2023-01-24 21:33:27 -0300

    test(green): should render with the passed title

--- packages/flip-card/flip-card.tsx
+++ packages/flip-card/flip-card.tsx
@@ -2,18 +2,20 @@ import React, { ReactNode } from 'react';
 import { styled } from '@mui/material';
 import Box from '@mui/material/Box';
 
-export interface FlipCardProps {}
+export interface FlipCardProps {
+  title: string;
+}
 
 const FlipCardRoot = styled(Box)({});
 const FlipCardContainer = styled(Box)({});
 const FlipCardFront = styled(Box)({});
 const FlipCardBack = styled(Box)({});
 
-export function FlipCard(props: FlipCardProps) {
+export function FlipCard({ title }: FlipCardProps) {
   return (
     <FlipCardRoot>
       <FlipCardContainer>
-        <FlipCardFront></FlipCardFront>
+        <FlipCardFront>{title}</FlipCardFront>
         <FlipCardBack></FlipCardBack>
       </FlipCardContainer>
     </FlipCardRoot>
