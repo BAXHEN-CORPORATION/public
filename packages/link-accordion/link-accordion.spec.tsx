6773df34ed2412c8cc605298b2851a09b3cb810e
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2023-02-27 12:16:24 -0300

    test(red): should not show the links if the title is not clicked and the links are passed

--- packages/link-accordion/link-accordion.spec.tsx
+++ packages/link-accordion/link-accordion.spec.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BasicLinkAccordion } from "./link-accordion.composition";
-import { title } from "./test/data";
+import { links, title } from "./test/data";
 
 it("should render with the correct title text", () => {
   const { getByText } = render(<BasicLinkAccordion />);
   const rendered = getByText(title);
   expect(rendered).toBeTruthy();
+});
+
+it("should not show the links if the title is not clicked and the links are passed", () => {
+  const { getByText } = render(<BasicLinkAccordion />);
+
+  links.forEach(({ detail, label }) => {
+    const renderedLabel = getByText(label);
+    const renderedDetail = getByText(detail);
+
+    expect(renderedLabel).not.toBeVisible();
+    expect(renderedDetail).not.toBeVisible();
+  });
 });
--- packages/link-accordion/test/data.ts
+++ packages/link-accordion/test/data.ts
@@ -2,3 +2,9 @@ export const title = "This year (12 releases)";
 export const content =
   "We provide front-end challenges that include professional web designs. These allow you to practice building websites in a realistic workflow. Taking these challenges will help you improve your skills, gain experience creating websites, and build up an incredible project portfolio. We also have a large community of developers, so it's a great way to meet other devs!";
 export const overrideContent = "this is an override example";
+export const links = [
+  { label: "16.5.0", detail: "6 hours ago" },
+  { label: "16.4.2", detail: "July, 7" },
+  { label: "16.4.2-alpha", detail: "Aug, 28" },
+  { label: "16.4.0", detail: "Aug, 12" },
+];
