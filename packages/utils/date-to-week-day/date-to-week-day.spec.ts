641f1f89c856fce9dce2b0786f6c1bd8328cece6
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2023-04-27 20:36:54 -0300

    test(green): all the functionalities are passing

--- packages/utils/date-to-week-day/date-to-week-day.spec.ts
+++ packages/utils/date-to-week-day/date-to-week-day.spec.ts
@@ -10,7 +10,7 @@ describe("dateToWeekDay", () => {
     expect(() => dateToWeekDay()).toThrow(TypeError);
   });
 
-  it('should return a string representing the date in the default locale ("pt-BR") when only the first argument is provided', () => {
+  it('should return a string representing the date in the default locale ("pt-BR") and default weekday "short" when only the first argument is provided', () => {
     expect(dateToWeekDay(new Date("2023-04-26"))).toBe("ter.");
   });
 
@@ -20,27 +20,15 @@ describe("dateToWeekDay", () => {
     );
   });
 
-  it("should return a string representing the date in all locales specified in an array when the second argument is an array of strings", () => {
-    // const locales = ["en-US", "de-DE", "es-ES"];
-    // const expected = ["4/26/2023", "26.04.2023", "26/04/2023"];
-    // expect(
-    //   locales.map((l) => dateToWeekDay(new Date("2023-04-26"), { locales: l }))
-    // ).toEqual(expected);
-  });
-
-  it('should return a string representing the date with the weekday in the default format ("short") when the second argument is not provided', () => {
-    // expect(dateToWeekDay(new Date("2023-04-26"))).toBe("26/04/2023");
-  });
-
   it('should return a string representing the date with the weekday in the "long" format when the second argument has the "weekday" key with a value of "long"', () => {
-    // expect(dateToWeekDay(new Date("2023-04-26"), { weekday: "long" })).toBe(
-    //   "quarta-feira, 26 de abril de 2023"
-    // );
+    expect(dateToWeekDay(new Date("2023-04-26"), { weekday: "long" })).toBe(
+      "terça-feira"
+    );
   });
 
   it('should return a string representing the date with the weekday in the "narrow" format when the second argument has the "weekday" key with a value of "narrow"', () => {
-    // expect(dateToWeekDay(new Date("2023-04-26"), { weekday: "narrow" })).toBe(
-    //   "26/04/2023"
-    // );
+    expect(dateToWeekDay(new Date("2023-04-26"), { weekday: "narrow" })).toBe(
+      "T"
+    );
   });
 });
