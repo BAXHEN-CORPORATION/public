551a64a7d16bd3f11c9a5a31c47c174f65fec22c
Leonardo Rebouças Batista <reboucas.beraby@gmail.com>
2023-05-01 12:07:28 -0300

    test(green): it should render with the correct default title

--- packages/fancy-upload/fancy-upload.tsx
+++ packages/fancy-upload/fancy-upload.tsx
@@ -1,9 +1,14 @@
 import React, { ReactNode } from "react";
 
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export interface FancyUploadProps {}
 
 export function FancyUpload({}: FancyUploadProps) {
-  return <Box minWidth="768px"></Box>;
+  return (
+    <Box minWidth="768px">
+      <Typography>Upload a File</Typography>
+    </Box>
+  );
 }
